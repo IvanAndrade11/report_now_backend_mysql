388508b8cab43a1bc9573fcda7d12264d256724f
Iván Andrade <87101510+IvanAndrade11@users.noreply.github.com>
2022-11-04 18:48:49 -0500

    :sparkles: Add new sql tables

--- src/bd.sql
+++ src/bd.sql
@@ -1,8 +1,29 @@
 create database report_now;
 use report_now;
+
+// TABLE USERS
 create table users (id int(50) not null auto_increment primary key, name varchar(35));
 ALTER TABLE `report_now`.`users` 
 ADD COLUMN `user` VARCHAR(45) NOT NULL AFTER `name`,
 ADD COLUMN `email` VARCHAR(45) NOT NULL AFTER `user`,
 ADD COLUMN `phone` VARCHAR(15) NULL DEFAULT NULL AFTER `email`,
 ADD COLUMN `password` VARCHAR(45) NOT NULL AFTER `phone`;
+
+// TABLE NEWS
+create table news (
+  id int(50) not null auto_increment primary key, 
+  title varchar(50),
+  description varchar(150),
+  likes int(10),
+  id_author int(50),
+  date varchar(10)
+);
+
+// TABLE IMAGES
+create table images (
+  id int(50) not null auto_increment primary key, 
+  title varchar(50),
+  src varchar(150),
+  id_user int(50),
+  id_news int(50)
+);
