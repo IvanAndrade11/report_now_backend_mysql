d460b316168a1f8b569416f6f8300fd27f68294e
Iván Andrade <87101510+IvanAndrade11@users.noreply.github.com>
2022-11-04 18:30:23 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_HOST = process.env.DB_HOST || "containers-us-west-42.railway.app";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "AguaPanela_11";
-export const DB_NAME = process.env.DB_NAME || "report_now";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_PASSWORD = process.env.DB_PASSWORD || "MLy3jf8EFifsZGzj2iXr";
+export const DB_NAME = process.env.DB_NAME || "railway";
+export const DB_PORT = process.env.DB_PORT || 7832;
