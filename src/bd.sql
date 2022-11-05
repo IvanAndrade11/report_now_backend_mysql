3a26a27c103a0598f5aaba29e859fec0b3552b7a
Iván Andrade <87101510+IvanAndrade11@users.noreply.github.com>
2022-11-04 19:11:44 -0500

    :bug: fix create users table

--- src/bd.sql
+++ src/bd.sql
@@ -4,10 +4,11 @@ use report_now;
 // TABLE USERS
 create table users (id int(50) not null auto_increment primary key, name varchar(35));
 ALTER TABLE `report_now`.`users` 
-ADD COLUMN `user` VARCHAR(45) NOT NULL AFTER `name`,
-ADD COLUMN `email` VARCHAR(45) NOT NULL AFTER `user`,
+ADD COLUMN `user` VARCHAR(50) NOT NULL AFTER `name`,
+ADD COLUMN `email` VARCHAR(50) NOT NULL AFTER `user`,
 ADD COLUMN `phone` VARCHAR(15) NULL DEFAULT NULL AFTER `email`,
-ADD COLUMN `password` VARCHAR(45) NOT NULL AFTER `phone`;
+ADD COLUMN `password` VARCHAR(300) NOT NULL AFTER `phone`,
+ADD COLUMN `admin` boolean NOT NULL AFTER `password`;
 
 // TABLE NEWS
 create table news (
